e6019be8cc6763cd54b5f375408b6710ea54bab9
conicorreag <conicorreag@uc.cl>
2023-06-30 22:47:59 -0400

    delete

--- src/migrations/20230701024016-delete_goals.js
+++ src/migrations/20230701024016-delete_goals.js
@@ -1,10 +1,11 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query('DELETE FROM "Neighbors"');
+    await queryInterface.sequelize.query('DELETE FROM "Territories"');
     await queryInterface.sequelize.query('DELETE FROM "Goals"');
     await queryInterface.sequelize.query('DELETE FROM "Games"');
     await queryInterface.sequelize.query('DELETE FROM "Kingdoms"');
-    await queryInterface.sequelize.query('DELETE FROM "Neighbors"');
     await queryInterface.sequelize.query('DELETE FROM "Shields"');
   },
 
