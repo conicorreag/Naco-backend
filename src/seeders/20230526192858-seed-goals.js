48b5ef6bc26823a773f742ef03dc425c2ff91f29
conicorreag <conicorreag@uc.cl>
2023-06-30 23:05:33 -0400

    id goal

--- src/seeders/20230526192858-seed-goals.js
+++ src/seeders/20230526192858-seed-goals.js
@@ -1,35 +1,35 @@
 module.exports = {
   up: (queryInterface) => queryInterface.bulkInsert('Goals', [
     {
-       
+      id: 1,
       userId: null,
       kingdomId: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
     {
-      
+      id: 2,
       userId: null,
       kingdomId: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
     {
-      
+      id: 3,
       userId: null,
       kingdomId: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
     {
-      
+      id: 4,
       userId: null,
       kingdomId: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
     {
-      
+      id: 5,
       userId: null,
       kingdomId: 5,
       createdAt: new Date(),
